35bf15560546f9a6315f0aaffbf355f27ce51743
Erick <erickm968@gmail.com>
2019-09-16 15:48:10 -0600

    Memory initializes in 0

--- Core/Memory.go
+++ Core/Memory.go
@@ -10,30 +10,28 @@ import (
 const numMemoryBlocks = 16
 
 // Define the blocks the build the memory
-type MemoryBlock struct{
-	Id      int
-	Data    int
-	// TODO AGREGAR EL ID DEL PROCESADOR QUE ESCRIBIÓ EL VALOR
+type MemoryBlock struct {
+	Id   int
+	Data int
 }
 
 // Define a memory with n blocks
-type Memory struct{
-	bus				*Bus
-	writeTime		time.Duration
-	readTime		time.Duration
-	storeDirectory 	string
-	gui 			GUI.MemoryGUI
+type Memory struct {
+	bus            *Bus
+	writeTime      time.Duration
+	readTime       time.Duration
+	storeDirectory string
+	gui            GUI.MemoryGUI
 	// Mutex to prevent reads and writes at the same time
-	mutex 			sync.Mutex
-	Block 			[numMemoryBlocks]MemoryBlock
+	mutex sync.Mutex
+	Block [numMemoryBlocks]MemoryBlock
 }
-
 
 func (memory *Memory) Init(bus *Bus, gui GUI.MemoryGUI) {
 
 	// Initialize memory to its default values
-	for i := 0; i < numMemoryBlocks; i++  {
-		memory.Block[i] = MemoryBlock{i, i + 1}
+	for i := 0; i < numMemoryBlocks; i++ {
+		memory.Block[i] = MemoryBlock{i, 0}
 	}
 
 	memory.bus = bus
@@ -43,7 +41,7 @@ func (memory *Memory) Init(bus *Bus, gui GUI.MemoryGUI) {
 	memory.gui = gui
 }
 
-func (memory *Memory) ReadBlock(blockId int) int{
+func (memory *Memory) ReadBlock(blockId int) int {
 
 	// Place a mutex to lock the memory
 	memory.mutex.Lock()
@@ -54,7 +52,7 @@ func (memory *Memory) ReadBlock(blockId int) int{
 
 }
 
-func (memory *Memory) WriteBlock(blockId int, data int){
+func (memory *Memory) WriteBlock(blockId int, data int) {
 
 	// Place a mutex to lock the memory
 	memory.mutex.Lock()
@@ -67,51 +65,50 @@ func (memory *Memory) WriteBlock(blockId int, data int){
 
 }
 
-func (memory *Memory) Run(clock *chan bool){
+func (memory *Memory) Run(clock *chan bool) {
 
 	// Store initial state
 	memory.showState()
 
-	for{
+	for {
 
 		select {
-			case <- *clock:
-				// This message is sent just before the tok
-				// so everyone should have ended their processes
-				go memory.showState()
+		case <-*clock:
+			// This message is sent just before the tok
+			// so everyone should have ended their processes
+			go memory.showState()
 
-				// Wait for a message to arrive
-			case e := <- memory.bus.EventBus[4]:
-				// Solve the message in another thread so we dont miss any incoming msg
-				go memory.solveEvent(e)
+			// Wait for a message to arrive
+		case e := <-memory.bus.EventBus[4]:
+			// Solve the message in another thread so we dont miss any incoming msg
+			go memory.solveEvent(e)
 		}
 	}
 }
 
-
-func (memory *Memory) solveEvent(e *Event){
+func (memory *Memory) solveEvent(e *Event) {
 
 	isValidEvent := e.Action != INVALIDATE
 
 	//Penalize memory access
 	if e.Action == READING {
 		time.Sleep(memory.readTime)
-	} else if e.Action == WRITING{
+	} else if e.Action == WRITING {
 		time.Sleep(memory.writeTime)
 	}
 
 	println(e.getInfo() + " into memory")
 	// If its a valid event and it hasn't been solved
-	if isValidEvent && !e.isSolved(){
+	if isValidEvent && !e.isSolved() {
 
 		// If its a write hit, write the desired block
-		if e.Action == WRITING && e.Result == HIT{
+		if e.Action == WRITING && e.Result == HIT {
 			memory.WriteBlock(e.TargetMemoryBlock.Id, e.TargetMemoryBlock.Data)
 
 			// If its a read miss try to return the value
-		}else if e.Action == READING && e.Result == MISS{
+		} else if e.Action == READING && e.Result == MISS {
 			// Get data from the block
-			r := MemoryBlock{e.TargetMemoryBlock.Id,memory.ReadBlock(e.TargetMemoryBlock.Id)}
+			r := MemoryBlock{e.TargetMemoryBlock.Id, memory.ReadBlock(e.TargetMemoryBlock.Id)}
 			// Send the block
 			memory.bus.AnswerBus[e.CoreId] <- r
 
@@ -119,7 +116,7 @@ func (memory *Memory) solveEvent(e *Event){
 	}
 }
 
-func (memory *Memory) showState(){
+func (memory *Memory) showState() {
 
 	memory.mutex.Lock()
 	memory.gui.Mutex.Lock()
